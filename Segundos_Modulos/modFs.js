ad865857843eb1a496b635e50d3180117de06bee
Daniel Moya <dmalvarado94@gmail.com>
2023-11-17 16:37:29 -0300

    Modulo fs() filesystem

---
+++ Segundos_Modulos/modFs.js
@@ -0,0 +1,6 @@
+// fs = file system - Manejo de archivos, leer, modificar
+// copiar, eliminar, cambiar nombre, etc.
+
+// Puedes escorger version síncronas  agregando Sync a sus nombres
+// EJ: fs.rename()  y fs.renameSync()
+
