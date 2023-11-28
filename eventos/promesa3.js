89093eedb319276ffa9b75b0c9c75d52ede9a00c
Daniel Moya <dmalvarado94@gmail.com>
2023-11-28 18:47:26 -0300

    Async

---
+++ eventos/promesa3.js
@@ -0,0 +1,48 @@
+// Async/Await Toda funcion con Async retorna una promesa
+function ordenarProducto(producto) {
+    return new Promise((resolve, reject) => {
+        console.log(`Ordenando: ${producto} del Menú`);
+        setTimeout(() => {
+            if (producto === 'taza'){
+                resolve('Ordenando una taza del menú');
+            } else {
+                reject('Este producto no está disponible')
+            }
+        }, 2000)
+    });
+ }
+
+ function procesarPedido(respuesta){
+    return new Promise((resolve) => {
+        console.log('Procesando respuesta...');
+        console.log(`La respuesta fue: "${respuesta}"`);
+        setTimeout(() => {
+            resolve('Gracias por tu compra. Pedido finalizado');
+        }, 4000);
+    })
+ }
+// Encadenamos promesas (Chaining process)
+//  ordenarProducto('taza')
+//     .then(respuesta => {
+//         console.log('Respuesta recibida');
+//         console.log(respuesta);
+//         return procesarPedido(respuesta);
+//     })
+//     .then(respuestaProcesada => {
+//         console.log(respuestaProcesada);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
+
+async function realizarPedido(producto){
+    try{
+    const respuesta = await ordenarProducto(producto);   
+    console.log('Respuesta recibida');
+    const respuestaProcesada = await procesarPedido(respuesta);
+    console.log(respuestaProcesada);
+    } catch (error) {
+        console.log(error);
+                    }
+}
+realizarPedido('taza');
